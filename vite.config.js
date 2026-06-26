6144d4cd696224b6252bc2edd7c81c64b3423b27
Himanshu <himanshubhatt070707@gmail.com>
2026-06-27 02:08:33 +0530

    fix vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/FreelanceSoftware/' : '/',
+  base: process.env.VERCEL ? '/' : process.env.NODE_ENV === 'production' ? '/FreelanceSoftware/' : '/',
   plugins: [react()],
 })
